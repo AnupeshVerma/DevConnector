311e62c966a0ff288b235231fa6cd4eb75add96d
Anupesh <Anupeshkverma121@gmail.com>
2024-08-19 21:36:39 +0530

    Add/Remove comments feature added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,6 +7,7 @@ const ProfileModel = require('../../models/Profile');
 const UserModel = require('../../models/User');
 const PostModel = require('../../models/Posts');
 
+
 // @route   POST api/posts
 // @desc    Create a post
 // @access  Private
@@ -95,6 +96,8 @@ router.delete("/:id", auth, async (req, res) => {
     }
 });
 
+// ####################### LIKES ########################
+
 // @route   PUT api/posts/like/:id
 // @desc    Like a post
 // @access  Private
@@ -143,5 +146,70 @@ router.put('/unlike/:id', auth, async (req, res) => {
         res.status(500).send("Server Error");
     }
 })
+
+// ################# COMMENTS ####################### 
+// @route   POST api/posts/comments/:id
+// @desc    Comment on a post
+// @access  Private
+router.post("/comment/:id", [auth, [
+    check('text', 'Text is required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+        return res.status(400).json({ errors: errors.array() });
+
+    // The select('-password') used to exclude the password being fetched
+    try {
+        const user = await UserModel.findById(req.user.id).select('-password');
+        const post = await PostModel.findById(req.params.id);
+
+        const newComments = {
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        };
+
+        post.comments.unshift(newComments);
+        await post.save();
+        res.json(post.comments);
+        console.log("A new comment created");
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+// @route   DELETE api/posts/comments/:id/:comment_id
+// @desc    Delete Comment on a post
+// @access  Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+    // The select('-password') used to exclude the password being fetched
+    try {
+        const user = await UserModel.findById(req.user.id).select('-password');
+        const post = await PostModel.findById(req.params.id);
+
+        // Pull out comment
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+        // Make sure comment exists
+        if(!comment)
+            return res.status(400).json({msg:'Comment does not exists'});
+
+        // Check user
+        if(comment.user.toString() !== req.user.id)
+            return res.status(401).json({msg:'User not authorized'});
+        // Delete comment
+        const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+        post.comments.splice(removeIndex, 1);
+        await post.save();
+
+        res.json(post.comments);
+        console.log("Comment deleted.");
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
 
 module.exports = router;
